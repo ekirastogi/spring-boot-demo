3183f80eaac4a2b626b1b508940bec7ac9dac853
Ekansh <ekansh.rastogi@intelligrape.com>
2016-01-20 00:25:26 +0530

    adding method type

--- src/main/java/com/ekiras/controller/HomeController.java
+++ src/main/java/com/ekiras/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.ekiras.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -16,7 +17,7 @@ public class HomeController {
         return "Hello World";
     }
 
-    @RequestMapping("/about")
+    @RequestMapping(value = "/about", method = RequestMethod.GET)
     public String about(){
         return "Spring Boot Introduction";
     }
